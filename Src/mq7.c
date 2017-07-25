1c2e3a092d41bc7a8746d4555be08bc885ba407d
Spaqin <3garfield@gmail.com>
2017-07-25 09:12:43 +0200

    forgot mq7 stuff lol'

---
+++ Src/mq7.c
@@ -0,0 +1,43 @@
+/*
+ * mq7.c
+ *
+ *  Created on: 15 lip 2017
+ *      Author: Spaqin
+ */
+
+#include "mq7.h"
+
+const char mq7_title[] = "CO";
+const char mq7_pattern[] = "%dppm";
+ADC_HandleTypeDef* mq7_adch;
+
+void mq7_init(ADC_HandleTypeDef* adch)
+{
+	Data_Display mq7_disp;
+	mq7_disp.callback_after_title = _mq7_callback;
+	mq7_disp.title.no_dot = mq7_title;
+	mq7_disp.data.dot = _mq7_data.co_text;
+	mq7_disp.dot_info = NO_DOTS;
+	mq7_disp.data_delay = 1024; //= 4s on a 32khz clock with 128 div prescaler
+	mq7_disp.title_delay = 512; // 2s
+	mq7_disp.level = 0;
+	disp_mgr_register(mq7_disp);
+	HAL_ADC_Start_DMA(adch, _mq7_data.raw_adc, 8);
+	mq7_adch = adch;
+}
+
+
+void _mq7_callback()
+{
+	uint16_t avg = 0;
+	for(int i = 0; i < 8; ++i)
+	{
+		avg += _mq7_data.raw_adc[i];
+	}
+	avg <<= 3;
+	uint32_t  voltage = 3300 * avg/4095;
+	// OK. This is not PPM. The problem is that the only thing with this sensor's datasheet you get,
+	// is ratio of resistance at current level and at 100ppm. I don't know either, so this will require further investigation.
+	// Known data, to be exact. This is also why I don't include a level indicator.
+	sprintf(_mq7_data.co_text, mq7_pattern, voltage);
+}
