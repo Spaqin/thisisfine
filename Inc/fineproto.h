ded22cfbc2b7d560e22f05ae10da94061b6366ba
Spaqin <3garfield@gmail.com>
2017-08-31 18:41:07 +0200

    lidl's sorbet ice cone is pretty bad

--- Inc/fineproto.h
+++ Inc/fineproto.h
@@ -1,6 +1,22 @@
 #ifndef FINEPROTO_H
 #define FINEPROTO_H
 #define QUEUE_SIZE 8
+#define FINEPROTO_HEADER 0xF1
+#define FINEPROTO_QUERY 0xF2
+
+#define QUERY_CAPABILITIES 0x01
+#define QUERY_SENSOR  0x02  //0xX2 - X is sensor ID
+#define QUERY_FWVER 0x04
+#define QUERY_CONTINOUS 0x05
+#define QUERY_HANDSHAKE 0xF1
+
+#ifndef BUILDDAY
+#define BUILDDAY 01
+#endif
+
+#ifndef BUILDMONTH
+#define BUILDMONTH 09
+#endif
 
 typedef enum Sensor {
   Temperature = 0x0,
@@ -27,7 +43,7 @@ typedef struct FineProtocol {
     FineMessage to_send; 
     uint16_t (*get_data_for[16])(void);  // callbacks for sensor data
     // Misc:
-    uint16_t continuous_timer; // counter for timer [in seconds]
+    uint16_t continuous_timer; // counter for timer [in seconds], btw i hate how it's not just "continous", but someone put an extra u in there for no reason at all goddamn
     uint8_t pri;  // protocol iterator for continous mode
 };
 
@@ -35,12 +51,13 @@ FineProtocol _fineproto;
 
 void fineproto_add_sensor(uint16_t (*get_data_for)(void));  // add sensor callback
 void fineproto_init();  // clean up the struct
-void parse_all_messages();  // goes through all messages in the queue
-void parse_message(FineMessage);  // sends an appropriate message or sets internal values accordingly.
-void _continous_setup();  // sets up the timer for continous mode
-void _continous_advance();  // sends and advances iterator for cont mode; called back when the timer calls
-void _got_message(); // callback for DMA finished (received a message)
-FineMessage _create_data_message(Sensor);  // tailors a message for given proto number
-inline uint8_t _calculate_checksum(FineMessage);
+void fp_parse_all_messages();  // goes through all messages in the queue
+void fp_parse_message(FineMessage);  // sends an appropriate message or sets internal values accordingly.
+void _fp_continuous_setup();  // sets up the timer for continous mode
+void _fp_continuous_stop();  // disables timer, cleans up
+void _fp_continuous_advance();  // sends and advances iterator for cont mode; called back when the timer calls
+void _fp_got_message(); // callback for DMA finished (received a message)
+FineMessage _fp_create_data_message(Sensor);  // tailors a message for given proto number
+inline uint8_t _fp_calculate_checksum(FineMessage);
 
 #endif
